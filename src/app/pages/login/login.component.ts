409cdc6526db7d03e09a0b73de9321c02b85af42
Flávio Oliveira <flavsoliver@gmail.com>
2023-07-28 12:25:13 -0300

    validation form login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/auth/auth.service';
 
@@ -8,37 +9,44 @@ import { AuthService } from 'src/app/modules/auth/auth.service';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
   isLoading: boolean = false;
   hide: boolean = true;
-  email: string = '';
-  password: string = '';
+  loginFail: boolean = false;
 
   constructor(
+    private formBuilder: FormBuilder,
     private readonly authService: AuthService,
     private readonly router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/),
+        ],
+      ],
+      password: ['', [Validators.required, Validators.pattern(/^.{6,}$/)]],
+    });
+  }
 
-  onLogin() {
-    // try {
-    //   this.isLoading = true;
-    //   const response = await this.authService.login(this.email, this.password);
-    //   console.log(response);
-    //   this.router.navigate(['']);
-    //   this.isLoading = false;
-    // } catch (error) {
-    //   console.log(error);
-    // }
+  async onLogin() {
     this.isLoading = true;
-    this.authService
-      .login(this.email, this.password)
-      .then(() => {
-        this.router.navigate(['']);
-        this.isLoading = false;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const result = await this.authService.login(
+      this.loginForm?.controls['email'].value,
+      this.loginForm?.controls['password'].value
+    );
+    if (result) {
+      this.router.navigate(['']);
+      this.isLoading = false;
+    } else {
+      this.loginFail = true;
+      this.isLoading = false;
+    }
+
+    return result;
   }
 }
