069ae075b3ecce598e08528bd901b199e44adda8
Flávio Oliveira <flavsoliver@gmail.com>
2023-08-04 00:35:14 -0300

    insert import material module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BarNavigationComponent } from './components/bar-navigation/bar-navigation.component';
+import { MaterialModule } from '../app-material.module';
 
 @NgModule({
-  imports: [CommonModule],
   declarations: [BarNavigationComponent],
+  imports: [CommonModule, MaterialModule],
 })
 export class SharedModule {}
