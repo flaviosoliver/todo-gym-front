93febd25c222e40c81567a638b002876df9961a7
Flávio Oliveira <flavsoliver@gmail.com>
2023-08-04 00:33:54 -0300

    update training interface

---
+++ src/app/modules/shared/interface/training.interface.ts
@@ -0,0 +1,12 @@
+export interface Training {
+  id: string;
+  exerciseId: string;
+  series: string;
+  repetitions: string;
+  load: string;
+  notes: string;
+  done: true;
+  exerciseName?: string;
+  exerciseImage?: string;
+  exerciseVideo?: string;
+}
