768158e779811b4a14694d6ddae7f5b114aee801
Flávio Oliveira <flavsoliver@gmail.com>
2023-08-04 00:36:14 -0300

    options menu

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/modules/auth/auth.service';
+import { TokenService } from 'src/app/modules/shared/token.service';
+import { User } from 'src/app/modules/users/interface/user.interface';
+import { UsersService } from 'src/app/modules/users/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  user: User | undefined;
 
-  constructor() { }
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly authService: AuthService,
+    private readonly tokenService: TokenService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit() {
+    this.getUser(this.userId);
   }
 
+  get userId(): string {
+    return this.authService.getUserId();
+  }
+
+  async getUser(id: string) {
+    const user = await this.usersService.getUser(id);
+    if (user) {
+      this.user = user;
+    }
+  }
+
+  routeTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  logout() {
+    this.tokenService.clearToken();
+    this.authService.clearUserId();
+    this.authService.clearEmail();
+    window.location.reload();
+  }
 }
