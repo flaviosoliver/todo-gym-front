2f48df348828ab9e7b8455d3ec5200fac2129c5a
Flávio Oliveira <flavsoliver@gmail.com>
2023-08-14 09:46:09 -0300

    response interceptor

---
+++ src/app/modules/shared/interceptor/response.interceptor.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpEventType,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class ResponseInterceptor implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap((event) => {
+        if (event.type === HttpEventType.Response) {
+          console.log('Resposta interceptada:', event);
+        }
+      })
+    );
+  }
+}
