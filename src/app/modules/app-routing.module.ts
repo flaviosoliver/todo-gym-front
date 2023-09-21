f8bdcf0c9b49bc8d5992ba3c9c5145eeb05695a0
Flávio Oliveira <flavsoliver@gmail.com>
2023-09-21 16:43:29 -0300

    routing

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -8,31 +8,73 @@ import { NewUserComponent } from '../pages/new-user/new-user.component';
 import { LoggedGuard } from './auth/guard/logged.guard';
 import { ProfileComponent } from '../pages/profile/profile.component';
 import { ExercisesListComponent } from '../pages/exercises-list/exercises-list.component';
+import { PlansComponent } from '../pages/plans/plans.component';
+import { PlanItemComponent } from '../pages/plans/components/plan-item/plan-item.component';
+import { ExpiresComponent } from '../pages/expires/expires.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     canActivate: [AuthGuard],
+    title: 'To-Do Gym | Home',
   },
   {
     path: 'profile/:userId',
     component: ProfileComponent,
     canActivate: [AuthGuard],
+    title: 'To-Do Gym | Meu Perfil',
   },
   {
     path: 'exercises',
     component: ExercisesListComponent,
-    children: [{ path: '', component: ExercisesListComponent }],
     canActivate: [AuthGuard],
+    title: 'To-Do Gym | Exercícios',
+  },
+  {
+    path: 'plans',
+    component: PlansComponent,
+    canActivate: [AuthGuard],
+    title: 'To-Do Gym | Meus Planos de Treino',
+  },
+  {
+    path: 'plans/expires',
+    component: ExpiresComponent,
+    canActivate: [AuthGuard],
+    title: 'To-Do Gym | Planos de Treino Expirados',
+  },
+  {
+    path: 'plans/form/:id',
+    component: PlanItemComponent,
+    canActivate: [AuthGuard],
+    title: 'To-Do Gym | Editar Plano de Treino',
+  },
+  {
+    path: 'plans/form',
+    component: PlanItemComponent,
+    canActivate: [AuthGuard],
+    title: 'To-Do Gym | Criar Novo Plano de Treino',
   },
   {
     path: '',
     component: AuthComponent,
     children: [
-      { path: '', redirectTo: 'login', pathMatch: 'full' },
-      { path: 'login', component: LoginComponent },
-      { path: 'new-user', component: NewUserComponent },
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+        title: 'To-Do Gym',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+        title: 'To-Do Gym | Entrar',
+      },
+      {
+        path: 'new-user',
+        component: NewUserComponent,
+        title: 'To-Do Gym | Criando Novo Usuário',
+      },
     ],
     canActivate: [LoggedGuard],
   },
